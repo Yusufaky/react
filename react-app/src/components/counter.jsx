d55c4af09b97c45cef50496f307a2bbe21a63626
Yusufaky <44864683+Yusufaky@users.noreply.github.com>
2022-03-04 21:11:19 +0000

    Props vs State

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,33 +2,51 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    counter: 0,
-    tags: ["teste1", "teste2", "teste3"],
+    value: this.props.value,
+    // tags: ["teste1", "teste2", "teste3"],
   };
+
+  handlerIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Teste</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handlerIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Adicionar
+        </button>
+        {/* {this.state.tags.length === 0 && "Colocar novas tags"} */}
+        {/* {this.renderTags()} */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge  m-2 bg-";
-    classes += this.state.counter === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { counter } = this.state;
+    const { value: counter } = this.state;
     return counter === 0 ? "Zero" : counter;
   }
+
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>Nao ha tags</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 }
 
 export default Counter;
